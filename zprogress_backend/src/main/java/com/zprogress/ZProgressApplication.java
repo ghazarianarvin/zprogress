02f0da34118eceea6c480081fe23eb4501304587
ghazarian.as@gmail.com <ghazarian.as@gmail.com>
2020-03-21 15:02:10 +0100

    zprogress backed inital commit

---
+++ zprogress_backend/src/main/java/com/zprogress/ZProgressApplication.java
@@ -0,0 +1,13 @@
+package com.zprogress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZProgressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZProgressApplication.class, args);
+	}
+
+}
---
+++ zprogress_backend/src/test/java/com/zprogress/ZProgressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zprogress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZProgressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
